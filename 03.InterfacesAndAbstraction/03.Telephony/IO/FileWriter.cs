377b0fcf02b1aa1f763231b128c82b1162f35316
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-06-28 00:04:01 +0300

    Abstraction solutions

---
+++ 03.InterfacesAndAbstraction/03.Telephony/IO/FileWriter.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Telephony.IO.Interfaces;
+
+namespace Telephony.IO;
+public class FileWriter : IWriter
+{
+    public void WriteLine(string line)
+    {
+        string filePath = "../../../test.txt";
+
+        using StreamWriter sw = new(filePath, true);
+
+        sw.WriteLine(line);
+    }
+}
